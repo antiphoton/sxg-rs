f3e391e9541a2ce3e31b4fb1aadcb325fd38041d
Boxiao Cao <9083193+antiphoton@users.noreply.github.com>
2022-03-01 14:57:27 -0800

    WIP

---
+++ sxg_rs/typescript/index.ts
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+type HeaderFields = Array<[string, string]>;
+
+export type AcceptFilter = 'PrefersSxg' | 'AcceptsSxg';
+
+export interface WasmRequest {
+  body: number[],
+  headers: HeaderFields,
+  method: 'Get' | 'Post',
+  url: string,
+}
+
+export interface WasmResponse {
+  body: number[];
+  headers: HeaderFields;
+  status: number;
+}
+
+export type PresetContent = ({ kind: 'direct' } & WasmResponse) | {
+  kind: 'toBeSigned',
+  url: string,
+  payload: WasmResponse,
+  fallback: WasmResponse,
+}
+
+export interface WasmWorker {
+  new(configYaml: string, certPem: string, issuerPem: string): WasmWorker;
+  createRequestHeaders(accept_filter: AcceptFilter, fields: HeaderFields): HeaderFields;
+  createSignedExchange(
+    fallbackUrl: string,
+    certOrigin: string,
+    statusCode: number,
+    payloadHeaders: HeaderFields,
+    payloadBody: Uint8Array,
+    nowInSeconds: number,
+    signer: (input: Uint8Array) => Promise<Uint8Array>,
+    subresourceFetcher: (request: WasmRequest) => Promise<WasmResponse>,
+    headerIntegrityGet: (url: string) => Promise<WasmResponse>,
+    headerIntegrityPut: (url: string, response: WasmResponse) => Promise<void>,
+  ): WasmResponse,
+  fetchOcspFromCa(fetcher: (request: WasmRequest) => Promise<WasmResponse>): Uint8Array,
+  getLastErrorMessage(): string;
+  servePresetContent(url: string, ocsp: string): PresetContent | undefined;
+  shouldRespondDebugInfo(): boolean;
+  validatePayloadHeaders(fields: HeaderFields): void,
+}
+
+export interface WasmFunctions {
+  init: () => void,
+  WasmWorker: WasmWorker,
+}
+
+export async function createWorker(wasmBytes: Buffer, configYaml: string, certPem: string, issuerPem: string) {
+  await wasm_bindgen(wasmBytes);
+  const {
+    init,
+    WasmWorker,
+  } = wasm_bindgen as WasmFunctions;
+  init();
+  return new WasmWorker(configYaml, certPem, issuerPem);
+};
